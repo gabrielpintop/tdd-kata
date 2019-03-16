6025606fd369dd0580590d07d89cb6796d2ad26e
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-03-16 00:34:49 -0500

    Step 5 - The test is fixed

--- Calculator.py
+++ Calculator.py
@@ -2,7 +2,9 @@ class Calculator:
     def add(self, string):
         if string == "":
             return 0
-        elif "," in string:
+        elif "," in string or "&" in string or ":" in string:
+            string = string.replace("&", ",")
+            string = string.replace(":", ",")
             numbers = string.split(",")
             sum = 0
             for num in numbers:
