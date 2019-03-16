b2c0b915e9b5ac1adce3ac0f2395ff0061827302
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-03-16 00:22:51 -0500

    Step 3 - The test with two numbers is fixed (Green)

--- Calculator.py
+++ Calculator.py
@@ -2,5 +2,7 @@ class Calculator:
     def add(self, string):
         if string == "":
             return 0
+        elif "," in string:
+            return int(string[0]) + int(string[2])
         else:
             return int(string)
